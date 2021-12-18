5eb9f9efce89fd2f21d657628081ed85c2f34e91
JardielCarlos <Jardiel.CarlosM@hotmail.com>
2021-12-18 13:17:45 -0300

    Lista com ordem sem espaço

--- Semana7_Q1.js
+++ Semana7_Q1.js
@@ -2,7 +2,7 @@ const createList = (Elements, Text) =>{
   const ul = document.createElement("ul")
   document.getElementById("conteudo").appendChild(ul)
   for (let c = 0; c < Elements; c++){
-    const texto = document.createTextNode(Text)
+    const texto = document.createTextNode(Text + (c+1))
     const li = document.createElement("li")
     li.appendChild(texto)
     ul.appendChild(li)
